3ad22797357863f3fd73687be5b2bef16101ed70
Norbert V <norbert@varney.de>
2025-05-09 09:30:51 +0200

    formating

--- edurent/Controller/Daterange.php
+++ edurent/Controller/Daterange.php
@@ -12,7 +12,7 @@
     
 ?>
 
- <!-- JQuery for daterangepicker -->
+<!-- JQuery for daterangepicker -->
 <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
 <!-- Moment for daterangepicker -->
 <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
