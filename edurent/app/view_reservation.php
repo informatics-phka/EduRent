d1f26b75cc0d9be64ec5520cfb6f0ba9d774eb30
Norbert V <norbert@varney.de>
2025-05-15 15:39:24 +0200

    Update view_reservation.php
    add buttons and javascript

--- edurent/app/view_reservation.php
+++ edurent/app/view_reservation.php
@@ -137,15 +137,41 @@ if ($is_superadmin) {
         ?>
         <br>
         <!-- Buttons -->
-        <form method="POST" action="edit_reservation" style="display:inline;">
-            <input type="hidden" name="reservation_id" value="<?php echo $reservation_id; ?>">
-            <div class='col-md-6 mb-3'>
-                <button type='submit' class='btn btn-success btn-block rounded mr-1 mb-1'>
+        <div class="d-flex flex-wrap gap-2 mb-3">
+            <?php if($status == 1) { //request?>
+                <button type='button' class='btn btn-danger rounded' onclick='order_cancel("<?php echo $reservation_id; ?>")'>
+                    <?php echo translate('word_cancel'); ?>
+                </button>
+                <button type='button' class='btn btn-success rounded' onclick='order_accept("<?php echo $reservation_id; ?>")'>
+                    <?php echo translate('word_confirm'); ?>
+                </button>
+            <?php } elseif($status == 2) { //Collectable ?>
+                <button type='button' class='btn btn-danger rounded' onclick='order_cancel("<?php echo $reservation_id; ?>")'>
+                    <?php echo translate('word_cancel'); ?>
+                </button>
+                <button type='button' class='btn btn-success rounded' onclick='order_pickup("<?php echo $reservation_id; ?>")'>
+                    <?php echo translate('status_3'); ?>
+                </button>
+            <?php } else if($status == 3) { //Retrieved ?>
+                <button type='button' class='btn btn-danger rounded' onclick='order_back("<?php echo $reservation_id; ?>")'>
+                    <?php echo translate('word_downgrade'); ?>
+                </button>
+                <button type='button' class='btn btn-success rounded' onclick='order_retour("<?php echo $reservation_id; ?>")'>
+                    <?php echo translate('word_return'); ?>
+                </button>
+            <?php } else if($status == 4 || $status == 6) { //Completed or Cancelled ?>
+                <button type='button' class='btn btn-danger rounded' onclick='order_remove("<?php echo $reservation_id; ?>")'>
+                    <?php echo translate('word_delete'); ?>
+                </button>
+            <?php }?>
+
+            <form method="POST" action="edit_reservation" style="display:inline;">
+                <input type="hidden" name="reservation_id" value="<?php echo $reservation_id; ?>">
+                <button type='submit' class='btn btn-warning rounded'>
                     <?php echo translate('word_editBig'); ?>
                 </button>
-            </div>
-        </form>
-
+            </form>
+        </div>
     </div>
 </body>
 
@@ -155,22 +181,22 @@ if ($is_superadmin) {
 
 <script>
 	document.addEventListener('DOMContentLoaded', () => {
-    // display current page in navbar
-    const links = document.querySelectorAll('#navbarMenu .nav-link');
-    const currentPath = window.location.pathname.toLowerCase()
-        .replace(/^\/edurent\//, '')
-        .replace(/\.php$/, '');
+        // display current page in navbar
+        const links = document.querySelectorAll('#navbarMenu .nav-link');
+        const currentPath = window.location.pathname.toLowerCase()
+            .replace(/^\/edurent\//, '')
+            .replace(/\.php$/, '');
 
-    links.forEach(link => {
-        const linkPath = link.getAttribute('href').toLowerCase();
+        links.forEach(link => {
+            const linkPath = link.getAttribute('href').toLowerCase();
 
-        if (currentPath == linkPath) {
-            link.classList.add('active');
-        } else {
-            link.classList.remove('active');
-        }
+            if (currentPath == linkPath) {
+                link.classList.add('active');
+            } else {
+                link.classList.remove('active');
+            }
+        });
     });
-});
 
 	document.querySelectorAll(".collapse_me th").forEach(headerCell => {
 		headerCell.addEventListener("click", () => {
@@ -183,4 +209,87 @@ if ($is_superadmin) {
 			}
 		});
 	});
+
+    function order_cancel(reservation_id) {
+		location.href = "admini?cancel=" + reservation_id;
+	}
+
+    function order_remove(reservation_id) {
+		location.href = "admini?rem=" + reservation_id;
+	}
+
+	function order_accept(reservation_id) {
+		location.href = "admini?org=" + reservation_id;
+	}
+
+	function order_back(reservation_id) {
+		location.href = "admini?zu=" + reservation_id;
+	}
+
+	function order_retour(reservation_id) {
+		location.href = "admini?ret=" + reservation_id;
+	}
+
+    function order_pickup2(reservation_id, amount) {
+		var devices = "";
+		var typs = "";
+		var bouth = "";
+		for (var i = 0; i < amount; i++) {
+			if (devices != "") {
+				devices += ",";
+				typs += ",";
+				bouth += ",";
+			}
+			devices += $("#device" + i).val();
+			typs += document.getElementById("type_indicator" + i).innerText;
+			bouth += document.getElementById("type_indicator" + i).innerText + $("#device" + i).val();
+		}
+		location.href = "admini?abh=" + reservation_id + "&new_tags=" + devices + "&new_typs=" + typs + "&new=" + bouth;
+	}
+
+	function order_pickup(reservation_id) { //confirm the issued devices
+		var mymodal = $('#my_modal');
+		var titel = "<?php echo translate('word_order'); ?> #" + reservation_id;
+		mymodal.find('.modal-title').text(titel);
+
+		var orders = <?php echo is_null($orders) ? "2" : json_encode($orders); ?>;
+
+		var string = "";
+
+		$("#button_grey").show();
+		$("#button_green").show();
+		$("#button_red").show();
+		$("#button_yellow").hide();
+
+		if (orders == "2") {
+			string = '<center>Es konnten keine zugeordneten Geräte gefunden werden. Bitte untersuchen die die Reservierung in der MySQL Datenbank.</center>';
+			$("#button_red").hide();
+			$("#button_green").hide();
+		} else {
+			var names = orders[reservation_id][1].split('|');
+			var tag = orders[reservation_id][2].split('|');
+			var type_indicator = orders[reservation_id][3].split('|');
+			var geraete = "";
+			var amount = 0;
+
+			string += "<center>Welche Geräte-IDs wurden ausgegeben?<br>";
+
+			for (var i = 0; i < names.length; i++) {
+				string += "<div class='row no-gutters' style='text-align:center;'>";
+				string += "<div class='col'>";
+				string += names[i];
+				string += "</div>";
+				string += "<div class='col input-group'>";
+				string += "<span class='input-group-text' id='type_indicator" + i + "' name = 'type_indicator" + i + "'>" + type_indicator[i] + "</span>";
+				string += "<input type='text' maxlength='<?php echo $limits['device_tag']; ?>' class='form-control rounded' id='device" + i + "' name = 'device" + i + "' value='" + tag[i] + "'>";
+				string += "</div>";
+				string += "</div>";
+				amount++;
+			}
+			$("#button_red").attr("onclick", "order_pickup2(" + reservation_id + "," + amount + ")");
+			string += '</center>';
+		}
+		mymodal.find('.modal-body').html(string);
+		mymodal.modal('show');
+	}
 </script>
