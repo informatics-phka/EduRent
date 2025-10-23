288f90ae2af3559d7d24eabe19c0e7ac335099e2
Norbert V <norbert@varney.de>
2025-10-23 12:28:13 +0200

    layout fix

--- edurent/app/admini.php
+++ edurent/app/admini.php
@@ -580,6 +580,11 @@ if(exists_and_not_empty('org', $_GET)){ //was fetched
 			</div>
 			<br>
 			<br>
+	<?php
+
+	if ($result = mysqli_query($link, $sql)) {
+		if (mysqli_num_rows($result) > 0) {
+	?>		
 			<div class='table-responsive'>
 				<table class='table results'>
 					<thead>
@@ -599,11 +604,6 @@ if(exists_and_not_empty('org', $_GET)){ //was fetched
 						</tr>
 					</thead>
 					<tbody>
-	<?php
-
-	if ($result = mysqli_query($link, $sql)) {
-		if (mysqli_num_rows($result) > 0) {
-	?>		
 						<?php
 						while ($row = mysqli_fetch_array($result)) {
 						?>
@@ -734,13 +734,13 @@ if(exists_and_not_empty('org', $_GET)){ //was fetched
 	} else {
 		error_to_superadmin(get_superadmins(), $mail, "ERROR: Could not able to execute: " . $sql . ": " . mysqli_error($link));
 	}
+	mysqli_close($link);
 			?>
 		</div>
 	</div>
 </body>
 <?php
 echo $OUTPUT->footer();
-mysqli_close($link);
 ?>
 <script>
 	var added = 0;
